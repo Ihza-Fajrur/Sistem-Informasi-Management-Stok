37d804a3af89512b464608036224ae8132fd363f
HRFI-B <84057899+Ihza-Fajrur@users.noreply.github.com>
2022-04-14 09:01:59 +0700

    Database Backup

--- database backup/sokou_lampung.sql
+++ database backup/sokou_lampung.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 04, 2022 at 11:19 AM
+-- Generation Time: Apr 14, 2022 at 03:38 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 7.4.28
 
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `accounts` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
-  `tipe_user` varchar(10) NOT NULL,
+  `acc_type` varchar(10) NOT NULL,
   `email` varchar(100) NOT NULL,
   `user_photo` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -39,8 +39,216 @@ CREATE TABLE `accounts` (
 -- Dumping data for table `accounts`
 --
 
-INSERT INTO `accounts` (`username`, `password`, `tipe_user`, `email`, `user_photo`) VALUES
-('HRFI', '1', 'Admin', 'ihza.119140130@student.itera.ac.id', '\"C:\\Users\\ihzaf\\OneDrive\\Gambar\\Pas Photo\\Ihza Fajrur Rachman Hasani_119140130.jpg\"');
+INSERT INTO `accounts` (`username`, `password`, `acc_type`, `email`, `user_photo`) VALUES
+('HRFI', '1', 'Staff', 'ihza.119140130@student.itera.ac.id', '/profile_picture/Ihza Fajrur Rachman Hasani_119140130.jpg'),
+('IFRH', '1', 'Admin', 'ihza.119140130@student.itera.ac.id', '/profile_picture/Ihza Fajrur Rachman Hasani_119140130.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `bahan_cutting`
+--
+
+CREATE TABLE `bahan_cutting` (
+  `kode_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `jenis_barang` varchar(50) NOT NULL,
+  `warna` varchar(15) NOT NULL,
+  `ukuran_panjang` int(5) NOT NULL,
+  `ukuran_lebar` int(5) NOT NULL,
+  `harga_satuan` int(9) NOT NULL,
+  `total_harga` int(9) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bahan_cutting`
+--
+
+INSERT INTO `bahan_cutting` (`kode_barang`, `nama_barang`, `jenis_barang`, `warna`, `ukuran_panjang`, `ukuran_lebar`, `harga_satuan`, `total_harga`) VALUES
+('DTAL', 'DTF', 'DTF', 'ALL', 50, 50, 500, 1250000),
+('FBHI', 'FLOCK', 'BLUDRU', 'HITAM', 60, 28, 2300, 3864000),
+('FBOR', 'FLOCK', 'BLUDRU', 'ORANGE', 50, 60, 2300, 6900000),
+('FBPU', 'FLOCK', 'BLUDRU', 'PUTIH', 25, 33, 2300, 1897500),
+('FLRW', 'POLYFLEX', 'FOIL', 'RAINBOW', 50, 76, 1200, 4560000),
+('KOPA', 'KERTAS', 'OPAQU', 'POLOS', 8, 1, 18000, 144000),
+('PUAM', 'POLYFLEX', 'PU', 'ABU MUDA', 40, 25, 1000, 1000000),
+('PUBM', 'POLYFLEX', 'PU', 'BIRU MUDA', 35, 35, 1000, 1225000),
+('PUGO', 'POLYFLEX', 'PU', 'GOLD', 50, 76, 1000, 3800000),
+('PUHI', 'POLYFLEX', 'PU', 'HITAM', 50, 97, 1000, 4850000),
+('PUHM', 'POLYFLEX', 'PU', 'HIJAU MUDA', 50, 50, 1000, 2500000),
+('PUKU', 'POLYFLEX', 'PU', 'KUNING', 50, 100, 1000, 5000000),
+('PUME', 'POLYFLEX', 'PU', 'MERAH', 50, 87, 1000, 4350000),
+('PUMO', 'POLYFLEX', 'PU', 'MOCCA', 39, 78, 1000, 3042000),
+('PUPU', 'POLYFLEX', 'PU', 'PUTIH', 55, 28, 1000, 1540000),
+('PUUN', 'POLYFLEX', 'PU', 'UNGU', 40, 28, 1000, 1120000),
+('PUVY', 'POLYFLEX', 'PU', 'VIVID YELLOW', 50, 96, 1000, 4800000),
+('PVBM', 'POLYFLEX', 'PVC', 'BIRU MUDA', 37, 33, 800, 976800),
+('PVHI', 'POLYFLEX', 'PVC', 'HITAM', 30, 44, 800, 1056000),
+('PVHT', 'POLYFLEX', 'PVC', 'HIJAU TUA', 50, 45, 800, 1800000),
+('PVME', 'POLYFLEX', 'PVC', 'MERAH', 50, 75, 800, 3000000),
+('PVOM', 'POLYFLEX', 'PVC', 'ORANGE MUDA', 30, 39, 800, 936000),
+('PVPI', 'POLYFLEX', 'PVC', 'PINK', 50, 89, 800, 3560000),
+('RLS', 'POLYFLEX', 'REFLECTIVE', 'SILVER', 45, 39, 2000, 3510000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kaos_original`
+--
+
+CREATE TABLE `kaos_original` (
+  `kode_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `size` varchar(10) NOT NULL,
+  `bentuk_lengan` varchar(15) NOT NULL,
+  `desain` varchar(20) NOT NULL,
+  `warna` varchar(15) NOT NULL,
+  `jumlah_stok` int(5) NOT NULL,
+  `harga_satuan` int(15) NOT NULL,
+  `total_harga` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kaos_original`
+--
+
+INSERT INTO `kaos_original` (`kode_barang`, `nama_barang`, `size`, `bentuk_lengan`, `desain`, `warna`, `jumlah_stok`, `harga_satuan`, `total_harga`) VALUES
+('BB1F2H', 'KAOS ORI', 'L', 'PENDEK', 'BBI', 'HITAM', 6, 250000, 1500000),
+('BB1G2H', 'KAOS ORI', 'XL', 'PENDEK', 'BBI', 'HITAM', 5, 250000, 1250000),
+('OR1C2H', 'KAOS ORI', 'S', 'PENDEK', 'ORIGINAL 1', 'HITAM', 7, 135000, 945000),
+('OR1D1P', 'KAOS ORI', 'M', 'PANJANG', 'ORIGINAL 1', 'PUTIH', 10, 145000, 1450000),
+('OR1E1P', 'KAOS ORI', 'ML', 'PANJANG', 'ORIGINAL 1', 'PUTIH', 5, 145000, 725000),
+('OR1F2P', 'KAOS ORI', 'L', 'PENDEK', 'ORIGINAL 1', 'PUTIH', 10, 135000, 1350000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kaos_polos`
+--
+
+CREATE TABLE `kaos_polos` (
+  `kode_barang` varchar(10) NOT NULL,
+  `nama_barang` varchar(100) NOT NULL,
+  `size` varchar(10) NOT NULL,
+  `jenis_kain` varchar(100) NOT NULL,
+  `bentuk_lengan` varchar(15) NOT NULL,
+  `bentuk_lingkar_leher` varchar(15) NOT NULL,
+  `warna` varchar(15) NOT NULL,
+  `jumlah_stok` int(5) NOT NULL,
+  `harga_satuan` int(15) NOT NULL,
+  `total_harga` int(15) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `kaos_polos`
+--
+
+INSERT INTO `kaos_polos` (`kode_barang`, `nama_barang`, `size`, `jenis_kain`, `bentuk_lengan`, `bentuk_lingkar_leher`, `warna`, `jumlah_stok`, `harga_satuan`, `total_harga`) VALUES
+('AE2DHI', 'KAOS ALS', 'M', 'COTTON CARDED 20S', 'PENDEK', 'O NECK', 'HITAM', 5, 75000, 375000),
+('AE2GHI', 'KAOS ALS', 'XL', 'COTTON CARDED 20S', 'PENDEK', 'O NECK', 'HITAM', 5, 75000, 375000),
+('AF2FHI', 'KAOS ALS', 'L', 'CATTON TC30S', 'PENDEK', 'O NECK', 'HITAM', 5, 70000, 350000),
+('AK2CHL', 'KAOS RMS', 'S', 'COTTON KOZE', 'PENDEK', 'O NECK', 'HIJAU LUMUT', 5, 75000, 375000),
+('AK2CKK', 'KAOS RMS', 'S', 'COTTON KOZE', 'PENDEK', 'O NECK', 'KUNING KENARI', 5, 75000, 375000),
+('AK2DBD', 'KAOS RMS', 'M', 'COTTON KOZE', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000),
+('AK2DOR', 'KAOS RMS', 'M', 'COTTON KOZE', 'PENDEK', 'O NECK', 'ORANGE', 5, 75000, 375000),
+('AK2FPU', 'KAOS RMS', 'L ', 'COTTON KOZE', 'PENDEK', 'O NECK', 'PUTIH', 5, 75000, 375000),
+('AK2GHI', 'KAOS RMS', 'XL', 'COTTON KOZE', 'PENDEK', 'O NECK', 'HITAM', 5, 75000, 375000),
+('AP2DKK', 'POLO ALS', 'M', 'COTTON VISCOSE PIQUE', 'PENDEK', 'BERKERAH', 'KUNING KENARI', 10, 85000, 850000),
+('AP2FBD', 'POLO ALS', 'L', 'COTTON VISCOSE PIQUE', 'PENDEK', 'BERKERAH', 'BIRU DONGKER', 10, 85000, 850000),
+('AP2HBB', 'POLO ALS', 'XXL', 'COTTON VISCOSE PIQUE', 'PENDEK', 'BERKERAH', 'BIRU BENHUR', 10, 85000, 850000),
+('AR3FHI', 'REGLAN ALS', 'L', 'COTTON COMBED 20S', '3/4', 'O NECK', 'HITAM', 10, 90000, 900000),
+('AT1DHI', 'TUNIK ALS', 'M', 'COTTON SPANDEX', 'PANJANG', 'O NECK', 'HITAM', 10, 90000, 900000),
+('AT1FHI', 'TUNIK ALS', 'L', 'COTTON SPANDEX', 'PANJANG', 'O NECK', 'HITAM', 10, 90000, 900000),
+('AT1GHI', 'TUNIK ALS', 'XL', 'COTTON SPANDEX', 'PANJANG', 'O NECK', 'HITAM', 10, 90000, 900000),
+('AV1CHI', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV1CPU', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PUTIH', 20, 70000, 1400000),
+('AV1DHA', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HIJAU ARMY', 5, 70000, 350000),
+('AV1DHI', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV1DKK', 'KAOS ALS', 'M ', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'KUNING KENARI', 5, 70000, 350000),
+('AV1DPU', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PUTIH', 20, 70000, 1400000),
+('AV1EHI', 'KAOS ALS', 'ML', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV1EHS', 'KAOS ALS', 'ML ', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HIJAU STABILO', 5, 70000, 350000),
+('AV1EMM', 'KAOS ALS', 'ML ', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'MERAH MAROON', 5, 70000, 350000),
+('AV1EPM', 'KAOS ALS', 'ML ', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PINK MAGENTA', 5, 70000, 350000),
+('AV1EPU', 'KAOS ALS', 'ML', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PUTIH', 20, 70000, 1400000),
+('AV1FHI', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV1FKK', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'KUNING KENARI', 5, 70000, 350000),
+('AV1FMM', 'HOODIE ALS', 'L ', 'COTTON FLEECE', 'PANJANG', 'JUMPER', 'MERAH MAROON', 10, 95000, 950000),
+('AV1FPM', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PINK MAGENTA', 5, 70000, 350000),
+('AV1FPU', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PUTIH', 20, 70000, 1400000),
+('AV1GAMt', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'ABU MISTY TUA', 5, 70000, 350000),
+('AV1GBD', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'BIRU DONGKER', 5, 70000, 350000),
+('AV1GHI', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV1GKK', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'KUNING KENARI', 5, 70000, 350000),
+('AV1GMT', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'MERAH TERANG', 5, 70000, 350000),
+('AV1GPM', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PINK MAGENTA', 5, 70000, 350000),
+('AV1GPU', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PUTIH', 20, 70000, 1400000),
+('AV1HAMm', 'KAOS ALS', 'XXL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'ABU MISTY MUDA', 5, 70000, 350000),
+('AV1HAMt', 'KAOS ALS', 'XXL ', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'ABU MISTY TUA', 5, 70000, 350000),
+('AV1HHI', 'KAOS ALS', 'XXL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV1HPU', 'KAOS ALS', 'XXL', 'COTTON COMBED 20S', 'PANJANG', 'O NECK', 'PUTIH', 20, 70000, 1400000),
+('AV2AHI', 'KAOS ALS', '1', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2APU', 'KAOS ALS', '1', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2BBT', 'KAOS ALS', '2', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU TOSKA', 5, 70000, 350000),
+('AV2BHI', 'KAOS ALS', '2', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2BPU', 'KAOS ALS', '2', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2CAG', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'ABU GELAP', 5, 70000, 350000),
+('AV2CBD', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 70000, 350000),
+('AV2CBTt', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU TURKISH TU', 5, 70000, 350000),
+('AV2CHI', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2CHN', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HIJAU NAVY', 5, 70000, 350000),
+('AV2CKB', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'KUNING BABY', 5, 70000, 350000),
+('AV2CMM', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'MERAH MAROON', 5, 70000, 350000),
+('AV2COR', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'ORANGE', 5, 70000, 350000),
+('AV2CPU', 'KAOS ALS', 'S', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2DBB', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU BENHUR', 5, 70000, 350000),
+('AV2DHI', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2DMC', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'MERAH CABE', 5, 70000, 350000),
+('AV2DMT', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'MERAH TERANG', 5, 70000, 350000),
+('AV2DPU', 'KAOS ALS', 'M', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2EAG', 'KAOS ALS', 'ML', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'ABU GELAP', 5, 70000, 350000),
+('AV2EHI', 'KAOS ALS', 'ML', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2EPU', 'KAOS ALS', 'ML', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2FBD', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 70000, 350000),
+('AV2FBT', 'KAOS ALS', 'L ', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU TOSKA', 5, 70000, 350000),
+('AV2FCK', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'COKLAT KOPI', 5, 70000, 350000),
+('AV2FCT', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'COKLAT TUA', 5, 70000, 350000),
+('AV2FHI', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2FKK', 'KAOS ALS', 'L ', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'KUNING KENARI', 5, 70000, 350000),
+('AV2FMM', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'MERAH MAROON', 5, 70000, 350000),
+('AV2FMT', 'KAOS ALS', 'L ', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'MERAH TERANG', 5, 70000, 350000),
+('AV2FPU', 'KAOS ALS', 'L', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2GBD', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 70000, 350000),
+('AV2GBT', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'BIRU TOSKA', 5, 70000, 350000),
+('AV2GHI', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2GHN', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HIJAU NAVY', 5, 70000, 350000),
+('AV2GHS', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HIJAU STABILO', 5, 70000, 350000),
+('AV2GMM', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'MERAH MAROON', 5, 70000, 350000),
+('AV2GPB', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PINK BABY', 5, 70000, 350000),
+('AV2GPU', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2HAMm', 'KAOS ALS', 'XXL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'ABU MISTY MUDA', 5, 70000, 350000),
+('AV2HHI', 'KAOS ALS', 'XXL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2HPU', 'KAOS ALS', 'XXL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2IHI', 'KAOS ALS', 'XXXL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2IPU', 'KAOS ALS', 'XXXL', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV2JHI', 'KAOS ALS', '0', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'HITAM', 10, 70000, 700000),
+('AV2JKK', 'KAOS ALS', '0', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'KUNING KENARI', 5, 70000, 350000),
+('AV2JPU', 'KAOS ALS', '0', 'COTTON COMBED 20S', 'PENDEK', 'O NECK', 'PUTIH', 10, 70000, 700000),
+('AV4GBB', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PANJANG MANSET', 'O NECK', 'BIRU BABY', 5, 70000, 350000),
+('AVn2GHI', 'KAOS ALS', 'XL', 'COTTON COMBED 20S', 'PENDEK', 'V NECK', 'HITAM', 5, 70000, 350000),
+('AX1DHI', 'SWEATER ALS', 'M', 'COTTON FLEECE', 'PANJANG', 'OBLONG', 'HITAM', 10, 95000, 950000),
+('AZ1GHI', 'HOODIE ALS', 'XL', 'COTTON FLEECE', 'PANJANG', 'JUMPER', 'HITAM', 10, 95000, 950000),
+('AZ1HHI', 'HOODIE ALS', 'XXL', 'COTTON FLEECE', 'PANJANG', 'JUMPER', 'HITAM', 10, 95000, 950000),
+('GS1CHI', 'KAOS GM', 'S', 'COTTON COMBED 24S', 'PANJANG', 'O NECK', 'HITAM', 5, 75000, 375000),
+('GS1DBD', 'KAOS GM', 'M', 'COTTON COMBED 24S', 'PANJANG', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000),
+('GS1FBD', 'KAOS GM', 'L', 'COTTON COMBED 24S', 'PANJANG', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000),
+('GS1FHI', 'KAOS GM', 'L', 'COTTON COMBED 24S', 'PANJANG', 'O NECK', 'HITAM', 5, 75000, 375000),
+('GS2DBD', 'KAOS GM', 'M', 'COTTON COMBED 24S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000),
+('GS2FBD', 'KAOS GM', 'L', 'COTTON COMBED 24S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000),
+('GS2FHI', 'KAOS GM', 'L', 'COTTON COMBED 24S', 'PENDEK', 'O NECK', 'HITAM', 5, 75000, 375000),
+('GS2GBD', 'KAOS GM', 'XL', 'COTTON COMBED 24S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000),
+('GS2GHI', 'KAOS GM', 'XL', 'COTTON COMBED 24S', 'PENDEK', 'O NECK', 'HITAM', 5, 75000, 375000),
+('GS2NBD', 'KAOS GM', '6', 'COTTON COMBED 24S', 'PENDEK', 'O NECK', 'BIRU DONGKER', 5, 75000, 375000);
 
 --
 -- Indexes for dumped tables
@@ -51,6 +259,24 @@ INSERT INTO `accounts` (`username`, `password`, `tipe_user`, `email`, `user_phot
 --
 ALTER TABLE `accounts`
   ADD PRIMARY KEY (`username`);
+
+--
+-- Indexes for table `bahan_cutting`
+--
+ALTER TABLE `bahan_cutting`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indexes for table `kaos_original`
+--
+ALTER TABLE `kaos_original`
+  ADD PRIMARY KEY (`kode_barang`);
+
+--
+-- Indexes for table `kaos_polos`
+--
+ALTER TABLE `kaos_polos`
+  ADD PRIMARY KEY (`kode_barang`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
